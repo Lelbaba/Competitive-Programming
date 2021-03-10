b98a5ecac554700d644d85b04f54afc97cbd00f5
Lelbaba <abdullahabrar9033@gmail.com>
2021-03-10 23:40:49 +0600

    Update Arituclation bridge.cpp

--- Graph/Arituclation bridge.cpp
+++ Graph/Arituclation bridge.cpp
@@ -1,62 +1,104 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
-struct graph
+using ll = long long;
+const int MONKE = 0;
+const int maxn = 1e5+5;
+//LOJ 1026
+class articulation_graph
 {
-	int n;
-	vector <int> t_in;
-	vector <int> low;
+	public:
+	vector <vector <int> > adj; //need to initialize
+	int timer;
+	vector <int> in,low;
 	vector <bool> vis;
-	vector <int> cut_points;
-	int timer = 0;
-	vector< vector <int> > edges;
-	void innit(){
-		t_in.resize(n);
-		low.resize(n);
-		vis.resize(n,false);
-		timer = 0;
-	}
-
-}g1;
-
-
-void dfs(int u,int p){
-	g1.timer++;
-	g1.t_in[u] = g1.timer;
-	g1.low[u] = g1.timer+1;
-	g1.vis[u] = true;
-	int children = 0;
-	for(auto v: g1.edges[u]){
-		if(v==p) continue;
-		if(!g1.vis[v]){ 
-			dfs(v,u);
-			g1.low[u] = min(g1.low[u],g1.low[v]);
+	set <int> cut_points;
+	vector <pair <int,int> > bridges;
+	void dfs(int node,int par=-1){
+		vis[node] = true;
+		in[node] = low[node] = timer++;
+		int kids = 0;
+		for(auto x:adj[node]){
+			if(x==par) continue;	// handle for directed graphs/multi-edges
+			if(vis[x]){
+				low[node] = min(low[node],in[x]);
+			}
+			else{
+				kids++;
+				dfs(x,node);
+				low[node] = min(low[node],low[x]);
+				if(in[node]<=low[x] && par!=-1){
+					cut_points.insert(node);
+				}
+				if(in[node]<low[x]){
+					bridges.push_back({node,x});
+				}
+			}
 		}
-		else g1.low[u] = min(g1.low[u],g1.t_in[v]);
-		children++;
+		if(par==-1 && kids>1){
+			cut_points.insert(node);
+		}
 	}
-	if(p==-1){
-		if(children>1) g1.cut_points.push_back(u);
-	} 
-	else if(g1.low[u]>=g1.t_in[u]) g1.cut_points.push_back(u);
+	vector <int> get_cutpoints(int n){
+		vector <int> ans;
+		cut_points.clear();
+		timer = 0;
+		low.assign(n,0);
+		in.assign(n,0);
+		vis.assign(n,false);
+		for(int i=0;i<n;i++){
+			if(!vis[i]) dfs(i);
+		}
+		for(auto x:cut_points) ans.push_back(x);
+		return ans;
+	}
+	vector <pair <int,int> > get_bridges(int n){
+		bridges.clear();
+		timer = 0;
+		low.assign(n,0);
+		in.assign(n,0);
+		vis.assign(n,false);
+		for(int i=0;i<n;i++){
+			if(!vis[i]) dfs(i);
+		}
+		return bridges;
+	}
+};
+
+
+void solve(){
+    int n,u,v;
+    cin>>n;
+    vector < vector <int> > adj(n);
+    string s;
+    for(int i=0;i<n;i++){
+    	cin>>u>>s;
+    	s = s.substr(1,s.size()-2);
+        int num = stoi(s);
+        for(int j=0;j<num;j++){
+            cin>>v;
+            adj[u].push_back(v);
+        }
+    }
+    articulation_graph G;
+    G.adj = adj;
+    auto bridges = G.get_bridges(n);
+    cout<<bridges.size()<<" critical links\n";
+    for(auto &x:bridges){
+        if(x.first>x.second) swap(x.first,x.second);
+    }
+    sort(bridges.begin(), bridges.end());
+    for(auto x:bridges){
+        cout<<x.first<<" - "<<x.second<<endl;
+    }
 }
 
-int main()
-{
-	int m;
-	cin>>g1.n>>m;
-	g1.edges.resize(g1.n);
-	for(int i=0;i<m;i++){
-		int u,v;
-		cin>>u>>v;
-		u--;v--;
-		g1.edges[u].push_back(v);
-		g1.edges[v].push_back(u);
-	}
-	g1.innit();
-	dfs(0,-1);
-	//for(auto x:g1.edges[4]) cout<<x<<endl;
-	for(auto x:g1.cut_points) cout<<x<<" "<<g1.t_in[x]<<" "<<g1.low[x]<<endl;
-	return 0;
+int main(){
+	int t;
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cin>>t;
+    for(int tc=1;tc<=t;tc++){
+        cout<<"Case "<<tc<<":\n";
+        solve();
+    }
 }
