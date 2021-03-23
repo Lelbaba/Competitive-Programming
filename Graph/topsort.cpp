cb4045f16bc11e42f9de94f6b54814b08d820cd9
Lelbaba <abdullahabrar9033@gmail.com>
2021-03-23 22:15:09 +0600

    Create topsort.cpp

---
+++ Graph/topsort.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef LEL
+#include "dbg.h"
+#else
+#define dbg(...) {/* temon kichu na; */}
+#endif
+using namespace std;
+using ll = long long;
+const int MONKE = 0;
+
+//untested 
+
+class top_sort
+{
+public:
+	vector <vector <int> > adj;
+	vector <int> in,order;
+	int n;
+	top_sort(int _n,vector <pair<int,int> > &edges){
+		n = _n;
+		adj.clear();
+		adj.resize(n);
+		in.resize(n);
+		for(auto &e:edges){
+			adj[e.first].push_back(e.second);
+			in[e.second]++;
+		}
+	}
+	void make_sorted(){
+		queue <int> q;
+		order.clear();
+		for(int i=0;i<n;i++){
+			if(!in[i]) q.push(i);
+		}
+		while(!q.empty()){
+			auto t = q.front();
+			dbg(t);
+			order.push_back(t);
+			q.pop();
+			for(auto v:adj[t]){
+				in[v]--;
+				dbg(v);
+				if(!in[v]){
+					q.push(v);
+				}
+			}
+		}
+	}
+};
+
+int main()
+{
+	int t;
+	fastio
+	cin>>t;
+	for(int tc=1;tc<=t;tc++){
+		int n,m;
+		cin>>n>>m;
+		vector <vector <int> >adj(n);
+		vector <pair<int,int> >edges(m);
+		for(auto &e:edges){
+			cin>>e.first>>e.second;
+			e.first--; e.second--;
+		}
+		top_sort graph(n,edges);
+		graph.make_sorted();
+		for(auto x:graph.order){
+			cout<<x+1<<" ";
+		}
+	}
+	return MONKE;
+}
