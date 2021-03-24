f54170341ba8bf6d3f269a2ac111921148defbd7
Lelbaba <abdullahabrar9033@gmail.com>
2021-03-24 22:12:55 +0600

    Combi template

---
+++ Math/Combinatorics_mod.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef LEL
+#include "dbg.h"
+#else
+#define dbg(...) {/* temon kichu na; */}
+#endif
+using namespace std;
+using ll = long long;
+const int MONKE = 0;
+
+
+ll inv_mod(ll a, ll m){
+    ll _m = m;
+    ll y = 0, x = 1;
+ 
+    if (m == 1)
+        return 0;
+ 
+    while (a > 1) {
+        ll q = a / m, t = m;
+        m = a % m, a = t;
+        t = y;
+        y = x - q * y;
+        x = t;
+    }
+    if (x < 0)
+        x += _m;
+    return x;
+}
+class combi
+{
+public:
+	ll mod = 1e9+7;
+	vector <ll> factorial,inv;
+	void init(ll n){
+		inv.reserve(n);
+		factorial.reserve(n);
+		factorial[0] = 1;
+		inv[0] = inv_mod(1,mod);
+		for(int i=1;i<=n;i++){
+			factorial[i] = (factorial[i-1]*i)%mod;
+			inv[i] = inv_mod(factorial[i],mod);
+		}
+	}
+	ll C(ll n,ll r){
+		if(r>n) return 0;
+		ll ans = 1;
+		ans = ans*factorial[n]%mod;
+		ans = ans*inv[r]%mod;
+		ans = ans*inv[n-r]%mod;
+		return ans;
+	}
+} com;
+
+int main()
+{
+	com.init(200005);
+	cout<<com.C(7,3);
+	return MONKE;
+}
