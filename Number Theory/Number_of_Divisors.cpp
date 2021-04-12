9f1bf0655bb1c81a54d681ae178e422f2df14183
Lelbaba <abdullahabrar9033@gmail.com>
2021-04-12 08:16:04 +0600

    Number of divisors added

--- Number Theory/Number_of_Divisors.cpp
+++ Number Theory/Number_of_Divisors.cpp
@@ -10,29 +10,30 @@
 using namespace std;
 using ll = long long;
 const int MONKE = 0;
-//CF 1512 G
+
 
 vector <bool> prime;
-vector <ll> primes,SOD,SPF_pow; //SPF_pow stores the greatest power of SPF 
+vector <ll> primes,NOD,SPF_cnt;
 
 void seive(ll n = 1<<20){
-    prime.assign(n+1,true), SPF_pow.resize(n+1), SOD.resize(n+1);
-    SOD[1] = 1;
+    prime.assign(n+1,true), NOD.resize(n+1), SPF_cnt.resize(n+1);
+    NOD[1] = 1;
     for(ll i=2;i<=n;i++){
         if(prime[i]) {
-        	SOD[i] = i+1, SPF_pow[i] = i;
+        	SPF_cnt[i] = 1,NOD[i] = 2;
             primes.push_back(i);
         }
         for(auto p:primes){
             if(p*i>n)   break;
             prime[p*i] = false;
+
             if(i%p == 0){
-            	SPF_pow[p*i] = p*SPF_pow[i];
-            	SOD[p*i] = SOD[i]/SOD[SPF_pow[i]] * (SOD[SPF_pow[i]] + SPF_pow[p*i]);
-            	break;
+            	SPF_cnt[p*i] = SPF_cnt[i] + 1;
+            	NOD[p*i] = NOD[i]/(SPF_cnt[i]+1) * (SPF_cnt[i]+2);
+            	break; 
             } else {
-            	SOD[p*i] = SOD[i]*SOD[p];
-            	SPF_pow[p*i] = p;
+            	NOD[p*i] = NOD[p]*NOD[i];
+            	SPF_cnt[p*i] = 1;
             }
         }
     }
@@ -41,16 +42,9 @@ void seive(ll n = 1<<20){
 int main()
 {
 	monke_flip
-	int n = 1e7+5,t;
-	seive(n);
-	vector <int> ans(n,-1);
-	for(int i=1;i<n;i++){
-		if(SOD[i]<n and ans[SOD[i]] == -1) ans[SOD[i]] = i;
-	}
-	cin>>t;
-	while(t--){
-		cin>>n;
-		cout<<ans[n]<<'\n';
+	seive();
+	for(int i=1;i<100;i++){
+		cout<<NOD[i]<<'\n';
 	}
 	return MONKE;
 }
