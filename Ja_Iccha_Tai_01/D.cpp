5cb92b4569c52634f48dbcfa8033dcaecc1065fd
Lelbaba <abdullahabrar9033@gmail.com>
2021-04-16 13:21:21 +0600

    Update D.cpp

--- Ja_Iccha_Tai_01/D.cpp
+++ Ja_Iccha_Tai_01/D.cpp
@@ -11,70 +11,50 @@ using namespace std;
 using ll = long long;
 const int MONKE = 0;
 using edge = pair <ll,ll>;
-struct dt
-{
-	ll first,second,cnt;
-	bool operator < (dt &rhs){
-		return first <rhs.first or (first == rhs.first and (second<rhs.second));
-	}
-};
-class djikstra{
-public:
-	ll n = -1;
-	vector <vector <edge> > adj;
-	vector <ll> D,path;
-	djikstra(vector < vector <edge> > &A){
-		n = A.size();
-		adj = A;
-		D.assign(n,LLONG_MIN);
-		path.assign(n,-1);
-	}
-	ll find_shortest_paths(ll s,ll f,ll t) {
-		priority_queue < edge, vector <edge>, greater<edge> > Q;
-		Q.emplace(0,s,1);
-		ll ans = 0;
-		while(!Q.empty()){
-
-			ll node = Q.top().second, d = Q.top().first, cnt = Q.top.cnt();
-			Q.pop();
-			if(d>t) continue;
-			for(auto e:adj[node]){
-				ll _next = e.second, dis = e.first;
-				if(dis+d<=t){
-					if(e.cnt>D[_next]){
-						D[_next] = e.cnt;
-						path[_next] = node;
-					}
-					Q.emplace(dis+d,_next);
-				}
-			}
-		}
-	}
-
-};
-
 
 int main()
 {
 	monke_flip
 	ll n, m, t;
 	cin >>n >>m >>t;
-	adj.resize(n);
+	vector <vector<edge> > adj(n);
 
 	for(int i=0; i<m; i++){
 		ll u, v, w;
 		cin >>u >>v >>w;
 		adj[u-1].emplace_back(w,v-1);
 	}
-	djikstra G(adj);
-	G.find_shortest_paths(n-1);
-	auto D = G.D;
-	path.assign(n,-1);
+	vector <vector <ll>> DP(n, vector<ll> (2,LLONG_MAX) );
+	vector <vector <int>> PATH(n, vector<int> (m+1) );
 
-	ll ans = dfs(0,n-1,t);
-	cout<<ans<<'\n';
-	for(ll i=0,node = 0;i<ans;i++, node = path[node]){
-		cout<<node+1<<" ";
+	DP[0][0] = 0;
+	
+	ll ans = -1;
+	
+	for(ll len = 0;len <=m;len++){
+		for(int i=0;i<n;i++)
+			DP[i][len+1&1] = LLONG_MAX;
+		if(DP[n-1][len&1]<=t) 
+			ans = len+1;
+
+		for(int i=0;i<n;i++){
+
+			if(DP[i][len&1] == LLONG_MAX) continue;
+			
+			for(auto x:adj[i]){
+				if(DP[i][len&1]+x.first < DP[x.second][len+1&1]){
+					DP[x.second][len+1&1] = DP[i][len&1] + x.first;
+					PATH[x.second][len+1] = i;
+				}
+			}
+		}
 	}
+	cout<<ans<<'\n';
+	vector <ll> path;
+    for(int i=ans, j = n-1; i>0; j = PATH[j][--i]){
+        path.push_back(j+1);
+    }
+    reverse(path.begin(), path.end());
+    for(auto x:path) cout<<x<<" ";
 	return MONKE;
 }
