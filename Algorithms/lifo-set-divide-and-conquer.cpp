14c3fee9d0081e10c7815fa0e519d300d9c74d72
lelbaba <abdullahabrar9033@gmail.com>
2023-02-11 03:03:10 +0600

    Lifo successful

--- Algorithms/lifo-set-divide-and-conquer.cpp
+++ Algorithms/lifo-set-divide-and-conquer.cpp
@@ -1,9 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 using LL = long long;
+/*
+ * https://judge.yosupo.jp/submission/125304
+ * https://codeforces.com/blog/entry/104443
+ *
+ * Dynamic Connectivity Offline by Divide and Conquer
+ * DSU with Rollback used as the LIFO-set
+ */
 const int N = 6e5 + 5, B = 450;
 LL a[N];
-vector <int> dbg;
+vector <pair <int, int>> dbg;
 int L, R;
 namespace dsu {
     int par[N], sz[N];
@@ -20,18 +27,17 @@ namespace dsu {
     void merge(int u, int v) {
         if(sz[u = root(u)] < sz[v = root(v)]) swap(u, v);
         if(u == v) return;
+        op.emplace_back(u, v);
         sz[u] += sz[v];
         a[u] += a[v];
         par[v] = u;
-        op.emplace_back(u, v);
     }
     void undo() {
         int u, v; tie(u, v) = op.back();
+        op.pop_back();
         sz[u] -= sz[v];
         a[u] -= a[v];
         par[v] = v;
-        op.pop_back();
-        dbg.pop_back();
     }
     void rollback(int t) {
         while(t < op.size()) {
@@ -42,7 +48,6 @@ namespace dsu {
 namespace up {
     int l[N], r[N], u[N], v[N], tm;
     void push(int _l, int _r, int _u, int _v) {
-        cout << tm << ": " << _l << ' ' << _r << ' ' << _u << ' ' << _v << '\n';
         l[tm] = _l, r[tm] = _r, u[tm] = _u, v[tm] = _v;
         tm++;
     }
@@ -57,11 +62,9 @@ namespace que {
 namespace edge_set {
     void push(int i) {
         dsu :: merge(up :: u[i], up :: v[i]);
-        dbg.push_back(i);
     }
-    void pop(int time) {
-        cout << "Popping " << L << ' ' << R << ' ' << time << '\n';
-        dsu :: rollback(time);
+    void pop(int t) {
+        dsu :: rollback(t);
     }
     int time() {
         return dsu :: op.size();
@@ -84,21 +87,15 @@ namespace dncq {
         update(idx, m + 1, r, node << 1 | 1);
     }
     void dfs(int l = 0, int r = que :: tm, int node = 1) {
-        cout << "Starting Interval :" << l << ' ' << r << '\n';
         int cur = edge_set :: time();
         for(int e: tree[node]) edge_set :: push(e);
-        L = l, R = r;
         if(l == r) {
-//            cout << l << ' ' << ":   ";
-//            for(int e: dbg) cout << e << ' ';
-//            cout << '\n';
             que :: ans[l] = edge_set :: query(que :: node[l]);
         } else {
             int m = l + r >> 1;
             dfs(l, m, node << 1);
             dfs(m + 1, r, node << 1 | 1);
         }
-
         edge_set :: pop(cur);
     }
 }
@@ -118,6 +115,7 @@ int main() {
         cin >> tp; 
         if(tp < 2) {
             cin >> u >> v;
+            if(u > v) swap(u, v);
             if(tp) {
                 int l = last[edg] + 1, r = cur;
                 up :: push(l, r, u, v);
@@ -133,11 +131,11 @@ int main() {
     }
     int r = que :: tm;
     for(auto &e: last) {
-        int l = e.second, u = e.first.first, v = e.first.second;
+        int l = e.second + 1, u = e.first.first, v = e.first.second;
         up :: push(l, r, u, v);
     }
     dsu :: init(n + 1);
     push_updates();
     dncq :: dfs();
-//    for(int i = 1; i <= que :: tm; i++) cout << que :: ans[i] << '\n';
+    for(int i = 1; i <= que :: tm; i++) cout << que :: ans[i] << '\n';
 }
