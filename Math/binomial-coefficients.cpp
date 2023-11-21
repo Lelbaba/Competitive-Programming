cc5b392a178ee0527cf1d0890e8cc21e0a6fd01d
lelbaba <abdullahabrar9033@gmail.com>
2023-11-22 00:23:21 +0600

    clean up

--- Math/binomial-coefficients.cpp
+++ Math/binomial-coefficients.cpp
@@ -6,17 +6,17 @@ const int N = 2e5+5;
 const int mod = 1e9+7;
 
 namespace com{
-    array <int, N+1> fact, inv, inv_fact;
+    LL fact[N], inv[N], inv_fact[N];
     void init(){
         fact[0] = inv_fact[0] = 1;
-        for(int i = 1; i <= N; i++){
+        for(int i = 1; i < N; i++){
             inv[i] = i == 1 ? 1 : (LL) inv[i - mod%i] * (mod/i + 1) % mod;
             fact[i] = (LL) fact[i-1] * i % mod;
             inv_fact[i] = (LL) inv_fact[i-1] * inv[i] % mod;
         }
     }
     LL C(int n,int r){
-        return (r < 0 or r > n) ? 0 : (LL) fact[n]*inv_fact[r] % mod * inv_fact[n-r] % mod;
+        return (r < 0 or r > n) ? 0 : fact[n] * inv_fact[r] % mod * inv_fact[n-r] % mod;
     }
 }
 int main(){
